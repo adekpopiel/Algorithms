990e31992d84cbd7d74819cae7e4723bc914df6c
Adrian Popiel <adrian.popiel@ocado.com>
2025-04-03 12:49:22 +0200

    Variable name changed

--- src/main/java/arrays/ArrayDuplicates.java
+++ src/main/java/arrays/ArrayDuplicates.java
@@ -5,14 +5,14 @@ import java.util.*;
 //https://www.geeksforgeeks.org/problems/find-duplicates-in-an-array/1?page=1&category=Arrays&difficulty=Easy&sortBy=submissions
 public class ArrayDuplicates {
     public List<Integer> findDuplicates(int[] arr) {
-        int[] elementsProcessed = new int[1000000];
+        int[] indexCounts = new int[1000000];
         List<Integer> output = new ArrayList<>();
         for (int i = arr.length-1; i >= 0; i--) {
             int elem = arr[i];
-            elementsProcessed[elem]++;
+            indexCounts[elem]++;
         }
-        for (int i = 0; i < elementsProcessed.length; i++) {
-            if (elementsProcessed[i] > 1) {
+        for (int i = 0; i < indexCounts.length; i++) {
+            if (indexCounts[i] > 1) {
                 output.add(i);
             }
         }
